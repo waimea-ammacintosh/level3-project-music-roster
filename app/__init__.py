f1efbe5f0d30bab3f6306a2de7949b727e2fc163
Aaron <ammacintosh@waimea.school.nz>
2026-08-26 10:57:02 +1200

    made sign in page

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,60 @@ def home():
 
 
 #-----------------------------------------------------------
-# Sign Up page - Sign User Up
+# Login page
+#-----------------------------------------------------------
+@app.get("/login")
+def show_login():
+
+    return render_template("pages/login.jinja")
+
+
+#-----------------------------------------------------------
+# Handle user login
+#-----------------------------------------------------------
+@app.post("/login")
+def process_login():
+    with connect_db() as db:
+        email = request.form.get('email', '').strip().lower()
+        password = request.form.get('password', '').strip()
+        
+
+        sql = """
+            SELECT user.email, user.pw_hash, user.first_name, user.last_name, user.id, user.role_id, role.name FROM user 
+            INNER JOIN role 
+            ON user.role_id = role.id
+            WHERE email = ?
+        """
+        params=(email,)
+        user = db.execute(sql, params).fetchone()
+
+
+        if not user:
+             flash(f"Unknown user", "error")
+             return redirect("/login")
+ 
+        if not check_password_hash(user["pw_hash"], password):
+            flash(f"Incorrect password", "error")
+            return redirect("/login")
+
+        session["logged_in"] = True
+        session["Role"] = user["role.name"]
+        session["User"] = {
+            "id": user["id"],
+            "first_name": user["first_name"],
+            "last_name": user["last_name"],
+            "email": user["email"]
+            }
+
+
+
+
+        flash("Signed In.", "success")
+
+        return redirect("/")
+
+#-----------------------------------------------------------
+# Register page - Sign User Up
 #-----------------------------------------------------------
 @app.get("/register")
 def register():
@@ -116,31 +169,42 @@ def process_new_user():
             return redirect("/")
 
         sql = """
+            SELECT email FROM user
+        """
+        params=()
+        users = db.execute(sql, params).fetchall()
+
+        for user in users:
+            if (email == user["email"]):
+                flash("A user with that email already exists", "error")
+                return redirect("/register")
+
+        sql2 = """
             INSERT INTO user (first_name, last_name, email, pw_hash, role_id)
             VALUES (?, ?, ?, ?, ?)
             RETURNING id;
             """
-        params = (first_name, last_name, email, pass_hash, role_id)
-        user = db.execute(sql, params).fetchone()
+        params2 = (first_name, last_name, email, pass_hash, role_id)
+        user = db.execute(sql2, params2).fetchone()
         user_id = next(iter(user.values()))
 
         for instrument in instruments:
 
-            sql2 = """
+            sql3 = """
                 SELECT id
                 FROM instrument
                 WHERE name = ?
             """
-            params2 = (instrument,)
-            instrument = db.execute(sql2, params2).fetchone()
+            params3 = (instrument,)
+            instrument = db.execute(sql3, params3).fetchone()
             instrument_id = next(iter(instrument.values()))
 
-            sql3 = """
+            sql4 = """
                 INSERT INTO instrumentUser (instrument_id, user_id)
                 VALUES (?,?)
             """
-            params3 = (instrument_id, user_id)
-            db.execute(sql3, params3)
+            params4 = (instrument_id, user_id)
+            db.execute(sql4, params4)
 
         flash("Account created.", "success")
         return redirect("/")
